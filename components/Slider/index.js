ec7869abec266167a9dc825a0e008eb79a8bed30
Leksa <surnacheva@ithub.ru>
2021-11-23 17:13:13 +0300

    until we done

--- components/Slider/index.js
+++ components/Slider/index.js
@@ -3,8 +3,38 @@ import tmp from "./template.js";
 class Slider extends HTMLElement {
     connectedCallback() {
         this.attachShadow({mode: "open"});
-        this.shadowRoot.innerHTML = tmp.render({});
+                this.shadowRoot.innerHTML = tmp.render({
+            type: this.getAttribute("type"),
+            color: this.getAttribute("rgb")
+        });
+        this.addEventListener("mousemove", this.handler);
     }
+    getRGB(h, s, v) {
+        let r, g, b, hue = h / 60, c = s * v, x;
+        x = c * (1 - Math.abs(hue % 2 - 1));
+        if (hue < 1 && hue >= 0) {
+            r = c; g = x; b = 0;
+        } else if (hue < 2) {
+            r = x; g = c; b = 0;
+        } else if (hue < 3) {
+            r = 0; g = c; b = x;
+        } else if (hue < 4) {
+            r = 0; g = x; b = c;
+        } else if (hue < 5) {
+            r = x; g = 0; b = c;
+        } else if (hue < 6) {
+            r = c; g = 0; b = x;
+        } else {
+            r = 0; g = 0; b = 0;
+        }
+        return `rgb(${Math.floor(r * 256)}, ${Math.floor(g * 256)}, ${Math.floor(b * 256)})`;
+    }
+    handler(e) {
+        const pos = e.clientX - e.target.getBoundingClientRect().x;
+        const h = (pos / e.target.offsetWidth) * 360;
+        document.body.querySelector("saturation-square").color = this.getRGB(h, 1, 1);
+    }
+
 }
 
 export default Slider;
--- components/Slider/template.js
+++ components/Slider/template.js
@@ -1,5 +1,6 @@
 export default {
     render(pr) {
+        console.log(pr);
         return `
             ${this.html(pr)}
             ${this.css(pr)}
@@ -25,6 +26,33 @@ export default {
                     position: absolute;
                     top: 0;
                     left: 0;
+                    background-image: ${
+                        pr.type === "hue" ?
+                            `linear-gradient(to right,
+                                             #f00 0%,
+                                             #ff0 16%,
+                                             #0f0 33%,
+                                             #0ff 50%,
+                                             #00f 66%,
+                                             #f0f 83%,
+                                             #f00 100%)`
+                        :  `linear-gradient(45deg, #ccc 25%, transparent 25%),
+                            linear-gradient(-45deg, #ccc 25%, transparent 25%),
+                            linear-gradient(45deg, transparent 75%, #ccc 75%),
+                            linear-gradient(-45deg, transparent 75%, #ccc 75%)`
+                    };
+                    background-size: ${pr.type === "hue" ? "cover" : "20px 20px"};
+                    background-position: ${pr.type === "hue" ? "center" : "0 0, 0 10px, 10px -10px, -10px 0"};
+                }
+                .bg::before {
+                    display: block;
+                    content: "";
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                    background-image: linear-gradient(to right, transparent 0%, ${pr.type === "hue" ? "transparent" : pr.color} 100%);
                 }
                 .thumb {
                     width: 8px;
